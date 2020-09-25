d1eae3b05303f3f962e6a96ac9273fcec9323e82
Helder Barbosa <helderlb@gmail.com>
2020-09-24 23:17:10 -0300

    roteamento

--- pessoa/src/main/java/com/si/dsi/pessoa/pessoa/controller/PessoaController.java
+++ pessoa/src/main/java/com/si/dsi/pessoa/pessoa/controller/PessoaController.java
@@ -1,17 +1,21 @@
 package com.si.dsi.pessoa.pessoa.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController("pessoa")
+@Controller
+@RequestMapping("usuarios")
 public class PessoaController {
 	
-
+	@GetMapping
+	public ResponseEntity teste() {
 	
-	@GetMapping(path = "todos")
-	public void teste() {
-	
+		System.out.println("testado");
 		
+		return new ResponseEntity(HttpStatus.OK);
 	}
 	
 }
